42fce96d346aef3d437bf79e9e65f671f51a0aa7
Md. Rony Hossain <rony191002@gmail.com>
2024-02-03 19:29:28 +0600

    Added my project

---
+++ src/client.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client started..");
+        Socket socket = new Socket("127.0.0.1", 22222 );
+        System.out.println("Client started..");
+        System.out.println("Client connected..");
+
+
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+
+        Scanner sc = new Scanner(System.in);
+        String message = sc.nextLine();
+        //sent  to server
+        oos.writeObject(message);
+        try {
+            //receive from server..
+            Object fromServer = ois.readObject();
+            System.out.println("From server: "+(String) fromServer);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
